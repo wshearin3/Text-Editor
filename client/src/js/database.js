3e82ea06ed3dd252a29f469d93e717376480f69b
Trey <trey.shearin3@gmail.com>
2024-06-03 00:35:55 -0400

    deleted TODO comments in database.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+
 export const putDb = async (content) => {
     console.log('PUT to database');
     const jateDb = await openDB('jate', 1);
@@ -23,7 +23,7 @@ export const putDb = async (content) => {
     console.log('Data added to the database', result);
 }
 
-// TODO: Add logic for a method that gets all the content from the database
+
 export const getDb = async () => {
     console.log('GET from database');
     const jateDb = await openDB('jate', 1);
