d8fa96b7c0e4db1d2c78fd6f2ff8e1607a9b5a1e
Trey <trey.shearin3@gmail.com>
2024-06-03 00:35:23 -0400

    created logic for get method in database.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -25,7 +25,14 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-    
+    console.log('GET from database');
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.get(1);
+    const result = await request;
+    console.log('Data pulled from database', result);
+    return result;
 };
 
 initdb();
