a8e9e24130b0007db20d8d81692602fb4b881a3c
Trey <trey.shearin3@gmail.com>
2024-06-03 00:30:01 -0400

    added method to accept content and add to database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,13 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-    
+    console.log('PUT to database');
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.put({id: 1, value: content});
+    const result = await request;
+    console.log('Data added to the database', result);
 }
 
 // TODO: Add logic for a method that gets all the content from the database
